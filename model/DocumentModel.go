3483aa22ef0eb73703a16317185dc429e0386fcd
Timofei <limjkeee@mail.ru>
2022-01-03 18:25:24 +0300

    add choose more than 1 book

--- model/DocumentModel.go
+++ model/DocumentModel.go
@@ -2,23 +2,61 @@ package model
 
 import (
 	"Library_project/repo"
+	"bytes"
 	"log"
+	"strings"
 )
 
-type Document struct {
-	DocId         uint16  `json:"doc_id"`
-	ReaderSurname string  `json:"reader_surname"`
-	BookName      string  `json:"book"`
-	Date          string  `json:"date"`
-	Price         float64 `json:"price"`
-	QuantityBook  uint16  `json:"quant"`
+func Discount(doc *repo.Document) {
+	if doc.QuantityBook == 2 || doc.QuantityBook == 3 {
+		doc.Price -= doc.Price * 10 / 100
+	} else if doc.QuantityBook >= 4 {
+		doc.Price -= doc.Price * 15 / 100
+	}
+}
+
+func StructSwitch(doc *repo.Document, docs *repo.DocumentForRest) {
+	doc.ReaderSurname = docs.ReaderSurname
+	doc.DocId = docs.DocId
+	doc.Date = docs.Date
+	doc.QuantityBook = docs.QuantityBook
+	doc.Price = docs.Price
+	CheckAndChangeInfo(docs, doc)
+}
+
+func CheckAndChangeInfo(docs *repo.DocumentForRest, doc *repo.Document) {
+	var Books []repo.Book
+	Books = GetBooks(Books)
+	books := docs.BookName
+	buffer := bytes.Buffer{}
+	var price uint16
+	var quant uint16
+	for _, i := range Books {
+		for _, j := range books {
+			if i.BookName == j {
+				buffer.WriteString(j + ", ")
+				price += i.Price
+				quant = quant + 1
+			} else {
+				log.Println("Something wrong")
+			}
+		}
+	}
+	doc.BookName = buffer.String()
+	doc.Price = float64(price)
+	doc.QuantityBook = quant
+	Discount(doc)
 }
 
 func SaveDocument(document *repo.Document) {
 	var Readers []repo.Reader
+	var Books []repo.Book
 	Readers = GetReaders(Readers)
-	if document.ReaderSurname == "" || document.BookName == "" || document.QuantityBook > 1 {
+	Books = GetBooks(Books)
+	if document.ReaderSurname == "" || document.BookName == "" {
 		log.Println("Пустые поля")
+	} else if document.QuantityBook > 5 {
+		log.Println("Нельзя брать более 5 книг")
 	} else {
 		for _, item := range Readers {
 			if item.Surname == document.ReaderSurname {
@@ -26,8 +64,12 @@ func SaveDocument(document *repo.Document) {
 					log.Println("Необходимо погасить долг прежде, чем брать новую книгу")
 				} else {
 					repo.SaveDocumentInDB(*document)
-					repo.DecreaseBookAvailableInDB(document.BookName)
-					repo.IncreaseReaderDebtInDb(document.ReaderSurname)
+					for _, i := range Books {
+						if strings.Contains(document.BookName, i.BookName) {
+							repo.DecreaseBookAvailableInDB(i.BookName)
+						}
+					}
+					repo.IncreaseReaderDebtInDb(document.ReaderSurname, document.QuantityBook)
 				}
 			}
 		}
@@ -35,9 +77,15 @@ func SaveDocument(document *repo.Document) {
 }
 
 func DeleteDocument(document *repo.Document) {
+	var Books []repo.Book
+	Books = GetBooks(Books)
+	for _, i := range Books {
+		if strings.Contains(document.BookName, i.BookName) {
+			repo.IncreaseBookAvailableInDB(i.BookName)
+		}
+	}
 	repo.DeleteDocumentInDb(document.ReaderSurname)
-	repo.IncreaseBookAvailableInDB(document.BookName)
-	repo.DecreaseReaderDebtInDb(document.ReaderSurname)
+	repo.DecreaseReaderDebtInDb(document.ReaderSurname, document.QuantityBook)
 }
 
 func GetDocuments(Documents []repo.Document) []repo.Document {
