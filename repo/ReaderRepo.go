3318b60cff6053c9bcd6c017022b684a2569dde1
Timofei <limjkeee@mail.ru>
2022-01-07 17:51:30 +0300

    close to final

--- repo/ReaderRepo.go
+++ repo/ReaderRepo.go
@@ -4,6 +4,7 @@ import (
 	"Library_project/other"
 	"database/sql"
 	"fmt"
+	"strconv"
 )
 
 type Reader struct {
@@ -32,10 +33,10 @@ func IncreaseReaderDebtInDb(surname string, value uint16) {
 	updDebt.Err()
 }
 
-func DecreaseReaderDebtInDb(surname string, value uint16) {
+func DecreaseReaderDebtInDb(surname string) {
 	db := other.ConnectDB()
 	defer db.Close()
-	updDebt := db.QueryRow("UPDATE `readers` set debt = debt-? WHERE surname = ?", value, surname)
+	updDebt := db.QueryRow("UPDATE `readers` set debt = debt-1 WHERE surname = ?", surname)
 	updDebt.Err()
 }
 
@@ -56,6 +57,21 @@ func GetReaderFromDB(Readers *[]Reader) {
 	defer db.Close()
 
 	get, err := db.Query("Select * from `readers` order by name")
+	other.CheckErr(err)
+
+	for get.Next() {
+		var reader Reader
+		err = get.Scan(&reader.Id, &reader.Name, &reader.Birthdate, &reader.Adress, &reader.Surname, &reader.Email, &reader.Debt)
+		other.CheckErr(err)
+		*Readers = append(*Readers, reader)
+	}
+}
+
+func GetReaderFromDBWithPages(Readers *[]Reader, page string) {
+	db := other.ConnectDB()
+	defer db.Close()
+	p, _ := strconv.Atoi(page)
+	get, err := db.Query(fmt.Sprintf("Select * from `readers` order by name LIMIT 5 OFFSET %d", (p-1)*5))
 	other.CheckErr(err)
 
 	for get.Next() {
